74b20361735ba1b4fd5bfb4c31389e88ef19a871
abiola <abiola.arobieke@gmail.com>
2024-08-09 12:08:27 -0700

    Add widthdraw, deposit and check_balance methods

--- atm.py
+++ atm.py
@@ -2,3 +2,13 @@ class Atm:
     def __init__(self, location, manage_by):
         self.location = location
         self.manage_by = manage_by
+
+
+    def withdraw(self):
+        pass
+
+    def deposit(self):
+        pass
+
+    def check_balance(self):
+        pass
