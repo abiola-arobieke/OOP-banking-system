a2f5fd6c3be968899ca08a57e21ebdd4d65088de
abiola <abiola.arobieke@gmail.com>
2024-08-09 13:47:50 -0700

    Add setters and getter for an account

--- banking_system/bank.py
+++ banking_system/bank.py
@@ -1,7 +1,19 @@
+from account import Saving, Current
+
+
 class Bank:
     def __init__(self, code, address):
         self.code = code
         self.address = address
+        self.accounts = []
+
+    @property
+    def account(self):
+        return self.accounts
+
+    @account.setter
+    def account(self, acc):
+        self.account = acc
 
     def getAccounts(self):
         pass
