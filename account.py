0630dbc8fd332f00089812481d1581b6cb04242b
abiola <abiola.arobieke@gmail.com>
2024-08-09 11:38:41 -0700

    Add account.py to the project

---
+++ account.py
@@ -0,0 +1,11 @@
+class Account:
+    def __init__(self, number, balance=0):
+        self.number = number
+        self.balance = balance
+    
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
+
