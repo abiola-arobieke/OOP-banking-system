efbf498280356d3c0986d7010bd9d6b7cd6e076a
abiola <abiola.arobieke@gmail.com>
2024-08-09 13:48:15 -0700

    Add getters and setters for bank

--- banking_system/account.py
+++ banking_system/account.py
@@ -1,8 +1,21 @@
 class Account:
+    """Class representing a bank account"""
+
     def __init__(self, number, balance=0):
         self.number = number
         self.balance = balance
-    
+        self.banks = []
+
+    @property
+    def bank(self):
+        return self.banks
+
+    @bank.setter
+    def bank(self, bank):
+        self.banks = bank
+        if not self in self.banks.account:
+            self.banks.append(self)
+
     def deposit(self):
         pass
 
@@ -11,10 +24,13 @@ class Account:
 
 
 class Saving(Account):
+    """Class representing a saving account"""
+    
     def __init__(self, number, balance=0):
         super().__init__(number, balance)
 
 
 class Current(Account):
+    """Class representing a current account"""
     def __init__(self, number, balance=0):
         super().__init__(number, balance)
