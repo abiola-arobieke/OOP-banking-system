d4e96ae85784e7f0b7b106b289fc842526f3e70b
abiola <abiola.arobieke@gmail.com>
2024-08-09 11:05:27 -0700

    Add bank.py to the project

---
+++ bank.py
@@ -0,0 +1,13 @@
+class Bank:
+    def __init__(self, code, address):
+        self.code = code
+        self.address = address
+
+    def getAccounts(self):
+        pass
+
+    def maintain(self):
+        pass
+
+    def manages(self):
+        pass
