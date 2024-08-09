5f7fca8314a824291c9f1be06b06a49bd1aad007
abiola <abiola.arobieke@gmail.com>
2024-08-09 15:13:53 -0700

    Modify the account class

--- banking_system/account.py
+++ banking_system/account.py
@@ -14,7 +14,7 @@ class Account:
     def bank(self, bank):
         self.banks = bank
         if not self in self.banks.account:
-            self.banks.append(self)
+            self.bank.append(self)
 
     def deposit(self):
         pass
@@ -25,12 +25,13 @@ class Account:
 
 class Saving(Account):
     """Class representing a saving account"""
-    
+
     def __init__(self, number, balance=0):
         super().__init__(number, balance)
 
 
 class Current(Account):
     """Class representing a current account"""
+
     def __init__(self, number, balance=0):
         super().__init__(number, balance)
