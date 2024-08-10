1080a2af5e9553a5820c6037d9edb45ffd7e69b1
abiola <abiola.arobieke@gmail.com>
2024-08-10 02:00:30 -0700

    Test the atm instance deposit methods)

--- banking_system/app.py
+++ banking_system/app.py
@@ -6,8 +6,8 @@ first_bank = Bank('80078', 'Onikan, Lagos')
 atm1 = Atm('Onikan, Lagos', first_bank)
 atm2 = Atm('Onikan, Lagos', first_bank)
 
-first_bank.add_atm(atm1)
-first_bank.add_atm(atm2)
+atm1.deposit(50000)
+atm1.deposit(400000)
 
-for atm in first_bank.atms:
-    print(atm.location)
+print(atm1.get_deposit())
+print(atm1.balance)
