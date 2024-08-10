15bd67c8811468c52aff7bfe7c03ac46b4c61951
abiola <abiola.arobieke@gmail.com>
2024-08-10 03:41:39 -0700

    Test transaction class

--- banking_system/app.py
+++ banking_system/app.py
@@ -1,13 +1,18 @@
 from bank import Bank
 from atm import Atm
+from account import Current, Saving
+from transaction import Transaction
 
 first_bank = Bank('80078', 'Onikan, Lagos')
+
+acct1 = Saving(23456729803)
+acct2 = Current(2416387892)
 
 atm1 = Atm('Onikan, Lagos', first_bank)
 atm2 = Atm('Onikan, Lagos', first_bank)
 
-atm1.deposit(50000)
-atm1.deposit(400000)
+tran1 = Transaction(3000, acct1, atm1)
+tran1 = Transaction(5000, acct1, atm1)
 
-print(atm1.get_deposit())
-print(atm1.balance)
+print(acct1.transaction)
+print(atm1.transaction)
