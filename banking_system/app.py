78060562cd31d9fe33f1664da157704bc21e6a30
abiola <abiola.arobieke@gmail.com>
2024-08-10 13:38:03 -0700

    Test transaction.py file

--- banking_system/app.py
+++ banking_system/app.py
@@ -5,14 +5,8 @@ from transaction import Transaction
 
 first_bank = Bank('80078', 'Onikan, Lagos')
 
-acct1 = Saving(23456729803)
+acct1 = Saving(23456729803, 2000)
 acct2 = Current(2416387892)
 
 atm1 = Atm('Onikan, Lagos', first_bank)
 atm2 = Atm('Onikan, Lagos', first_bank)
-
-tran1 = Transaction(3000, acct1, atm1)
-tran1 = Transaction(5000, acct1, atm1)
-
-print(acct1.transaction)
-print(atm1.transaction)
