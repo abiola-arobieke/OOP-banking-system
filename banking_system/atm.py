7c321cade1799a45deb2a0180be3fd16a1a40c9b
abiola <abiola.arobieke@gmail.com>
2024-08-11 08:00:30 -0700

    Modify ATM class

--- banking_system/atm.py
+++ banking_system/atm.py
@@ -1,4 +1,9 @@
+"""Module providing a managing the atm activities."""
+
+
 class Atm:
+    """Class representing a atm class"""
+
     def __init__(self, location, manage_by, balance=0):
         self.location = location
         self.manage_by = manage_by
@@ -20,12 +25,15 @@ class Atm:
 
     def deposit(self, amount):
         """Method for depositing funds in the ATM"""
+
         if amount <= 0:
             raise ValueError('You cannot funds cannot be less than 0')
         self.__deposits.append(amount)
         self.balance += amount
 
     def withdraw(self, amount):
+        """A method for withrawing funds from the atm"""
+
         if amount > self.balance:
             raise ValueError('You cannot withdraw above the atm balance')
         elif amount <= 0:
@@ -34,7 +42,18 @@ class Atm:
 
     def get_all_deposit(self):
         """Method for getting all depsoit"""
+
         return self.__deposits
 
     def get_transactions(self):
+        """Get all transaction from the atm"""
+
         return self.transaction
+
+    def check_balance(self):
+        """Method for checking the ATM balance"""
+
+        total = 0
+        for transaction in self.transaction:
+            total += transaction.amount
+        return self.deposit - total
