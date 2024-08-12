0a7c03acceb39a6e2ba91dc5169a1d0c1afaa043
abiola <abiola.arobieke@gmail.com>
2024-08-12 14:39:22 -0700

    Test customer and bank classes

--- banking_system/app.py
+++ banking_system/app.py
@@ -1,30 +1,26 @@
 from bank import Bank
 from atm import Atm
-from account import Current, Saving
 from customer import Customer
 
 first_bank = Bank('80078', 'Onikan, Lagos')
+gt_bank = Bank('80456', 'Berger, Lagos')
+
 cust1 = Customer('Van Persie', 'Surulere, Lagos')
+cust2 = Customer('John Travola', 'Ojodu, Lagos')
 
 first_bank.create_account(cust1, 3425674356, 4500)
+gt_bank.create_account(cust1, 2352789123)
+first_bank.create_account(cust2, 4352789123)
+
 
 atm1 = Atm('Onikan, Lagos', first_bank)
-atm2 = Atm('Onikan, Lagos', first_bank)
+atm1.deposit(50000)
+# atm2 = Atm('Onikan, Lagos', first_bank)
 
-acct1 = Saving(23456729803, 2000)
-acct2 = Current(2416387892)
+first_bank.add_atm('Dugbe, Ibadan')
+first_bank.add_atm('Challenge, Ibadan')
 
-cust1.request_loan(3000, 3425674356)
-cust1.request_loan(5000, 3425674356)
-
-first_bank.approve_loan(3425674356)
-first_bank.approve_loan(3425674356)
-
-
-for acct in cust1.account:
-    if acct.number == 3425674356:
-        print(acct.balance)
-        print(acct.book_balance)
-        print(acct.loans)
-
-print(first_bank.loans)
+for account in cust1.account:
+    if account.number == 3425674356:
+        account.withdraw(1000, atm1)
+        print(account.balance)
